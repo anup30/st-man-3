7dc33204b5bac06161c34259db12fc294926af59
anup30 <anupbarua30@gmail.com>
2025-03-16 23:05:40 +0600

    update

--- student_management/settings.py
+++ student_management/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-placeholder-key-change-in-production'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://st-man-3-3.onrender.com/']
 
 # Application definition
 
